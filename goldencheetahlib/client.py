b7aa925e761073b2b3be20973863abc672969f0b
Aart Goossens <aart@goossens.me>
2016-08-16 15:59:48 +0200

    NEW - Added docstrings to client.py

--- goldencheetahlib/client.py
+++ goldencheetahlib/client.py
@@ -11,19 +11,74 @@ from .constants import (
 
 
 class GoldenCheetahClient:
+        """Class that provides access to GoldenCheetah's REST API
+        Can be used to retrieve lists of and single activities, including raw data.
+        """
+
     def __init__(self, athlete=None, host=DEFAULT_HOST):
+        """Initialize GC client.
+
+        Keyword arguments:
+        athlete -- Full name of athlete
+        host -- the full host (including \'http://\')
+        """
         self.athlete = athlete
         self.host = host
 
     @lru_cache()
     def get_athletes(self):
+        """Get all available athletes
+        This method is cached to prevent unnecessary calls to GC.
+        """
         return pd.read_csv(self.host)
 
     def get_activity_list(self):
+        """Get activity list for client.athlete"""
         return self._request_activity_list(self.athlete)
+
+    def get_athlete_zones(self):
+        """Get athlete zones for client.athlete.
+        Not implemented yet.
+        """
+        pass
+
+    def get_activity_by_filename(self, filename):
+        """Get raw activity data for filename for self.athlete
+        This call is slow and therefore this method is memory cached.
+
+        Keyword arguments:
+        filename -- filename of request activity (e.g. \'2015_04_29_09_03_16.json\')
+        """
+        return self._request_activity_data(self.athlete, filename)
+
+    def get_activity_bulk(self, activities):
+        """Get raw activity data in bulk for several activities
+
+        Keyword arguments:
+        activities -- (slice of) activity list DataFrame
+        """
+        for index, filename in activities.filename.iteritems():
+            activity_data = self.get_activity_by_filename(filename)
+            activities.set_value(index, 'data', activity_data)
+        return activities
+
+    def get_last_activity(self):
+        """Get all activity data for the last activity
+
+        Keyword arguments:
+        """
+        last_activity = self.get_activity_list().iloc[-1]
+        last_activity.set_value('data', self.get_activity_by_filename(last_activity.filename))
+        return last_activity
 
     @lru_cache()
     def _request_activity_list(self, athlete):
+        """Actually do the request for activity list
+        This call is slow and therefore this method is memory cached.
+
+        Keyword arguments:
+        athlete -- Full name of athlete
+        """
         if not self.athlete:
             raise Exception('self.athlete not defined')
         activity_list = pd.read_csv(
@@ -41,11 +96,15 @@ class GoldenCheetahClient:
         activity_list['data'] = pd.Series(dtype=np.dtype("object"))
         return activity_list
 
-    def get_athlete_zones(self):
-        pass
-
     @lru_cache(maxsize=256)
     def _request_activity_data(self, athlete, filename):
+        """Actually do the request for activity filename
+        This call is slow and therefore this method is memory cached.
+
+        Keyword arguments:
+        athlete -- Full name of athlete
+        filename -- filename of request activity (e.g. \'2015_04_29_09_03_16.json\')
+        """
         if not self.athlete:
             raise Exception('self.athlete not defined')
         response = requests.get(self._activity_endpoint(athlete, filename)).json()
@@ -57,28 +116,25 @@ class GoldenCheetahClient:
         activity.drop('time', axis=1, inplace=True)
 
         return activity[[i for i in ACTIVITY_COLUMN_ORDER if i in activity.columns]]
-    
-    def get_activity_by_filename(self, filename):
-        return self._request_activity_data(self.athlete, filename)
-
-    def get_activity_bulk(self, activities):
-        for index, filename in activities.filename.iteritems():
-            activity_data = self.get_activity_by_filename(filename)
-            activities.set_value(index, 'data', activity_data)
-        return activities
-
-    def get_last_activity(self):
-        last_activity = self.get_activity_list().iloc[-1]
-        last_activity.set_value('data', self.get_activity_by_filename(last_activity.filename))
-        return last_activity
 
     def _athlete_endpoint(self, athlete):
+        """Construct athlete endpoint from host and athlete name
+
+        Keyword arguments:
+        athlete -- Full athlete name
+        """
         return '{host}{athlete}'.format(
             host=self.host,
             athlete=quote_plus(athlete)
         )
 
     def _activity_endpoint(self, athlete, filename):
+        """Construct activity endpoint from host, athlete name and filename
+
+        Keyword arguments:
+        athlete -- Full athlete name
+        filename -- filename of request activity (e.g. \'2015_04_29_09_03_16.json\')
+        """
         return '{host}{athlete}/activity/{filename}'.format(
             host=self.host,
             athlete=quote_plus(athlete),
